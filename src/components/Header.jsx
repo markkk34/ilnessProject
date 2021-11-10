48dd15ca7593ebb06bbce3c1dd5b58ddcf32fb3c
markkk34 <mark.pochtarev@mail.ru>
2021-11-10 13:51:47 +0300

    Added basic structure of the app

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () =>
+{
+    return (
+        <header className='header'>
+            Mark
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+const Profile = () =>
+{
+    return (
+        <div className='profile'>
+            <div className='account-info'>
+                account
+            </div>
+            <div className='account-posts'>
+                posts
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
